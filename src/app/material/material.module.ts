89435471a828e14da63ae3945b217174be327840
AdrianHerrera_ <neomach55@gmail.com>
2021-11-10 22:59:16 -0600

    material

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+// Modulos
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  exports: [
+    MatSidenavModule,
+  ],
+})
+export class MaterialModule { }
