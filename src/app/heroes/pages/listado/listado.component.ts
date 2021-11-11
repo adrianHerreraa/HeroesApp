464975b57fe7d880a92021fb0e961e16e9d3c8b2
AdrianHerrera_ <neomach55@gmail.com>
2021-11-11 13:25:43 -0600

    flexLayout

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,20 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styles: [
+    `
+      mat-card {
+        margin-top: 20px;
+      }
+    `
   ]
 })
 export class ListadoComponent implements OnInit {
+
+
+  heroesList: Heroe[] = [];
 
   constructor( private hService: HeroesService ) { }
 
   ngOnInit(): void {
     this.hService.getHeroes().subscribe(
-      (resp) => {
-        console.log(resp);
+      (heroes) => {
+        this.heroesList = heroes;
+      },
+      (err) => {
+        console.log('Error del servidor');
       }
     );
   }
